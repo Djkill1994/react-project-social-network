79dbc81f55e9beb27abc326ddbe798947822660a
Bruyok Vlad <djkill911@gmail.com>
2021-11-28 23:38:28 +0300

    Social network project start

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import logo from "../logoDJKILL.png";
+
+
+const Header = () => {
+    return (
+    <header className={'header'}>
+        <img src={logo} alt='no img'/>
+    </header>);
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,29 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src={'https://st.depositphotos.com/1288156/4466/i/600/depositphotos_44660695-stock-photo-himalaya-mountains-black-and-white.jpg'}/>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                My posts
+                <div>
+                    New post
+                </div>
+                <div>
+                    <div>
+                        Post 1
+                    </div>
+                    <div>
+                        Post 2
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Profile
