19e5aaf34359b7d55363b0743aa34446728facaf
Vlad <84898137+Djkill1994@users.noreply.github.com>
2022-01-24 05:25:53 +0300

    added componentDidUpdate

--- src/components/Profile/ProfileInfo/ProfileStatus.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus.jsx
@@ -26,6 +26,14 @@ class ProfileStatus extends React.Component {
         });
     };
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if(prevProps.status !== this.props.status){
+            this.setState({
+                status: this.props.status
+            })
+        }
+    }
+
     render() {
         return (
             <div>
